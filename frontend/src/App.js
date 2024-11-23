f163d7bf234fa0e9c979fa0350b3c91c0662c497
vipul-mhatre <mhatrevipul1518@gmail.com>
2024-11-23 23:33:05 +0530

    initialised frontend using react

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Tell Truth Tech4Stack
+        </p>
+    </div>
+  );
+}
+
+export default App;
